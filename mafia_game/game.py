8aba6d6969e054b9a5581c891d5ed1a48fc40524
Kirill Bozhko <kirillkirik96@gmail.com>
2020-01-07 00:16:09 +0300

    implemented messages

--- mafia_game/game.py
+++ mafia_game/game.py
@@ -17,27 +17,47 @@ class GamePhase(Enum):
     NIGHT = auto()
 
 
+class DayPhaseStage(Enum):
+    VOTING = auto()
+    HANGING = auto()
+    LAST_WORD = auto()
+
+
+class NightPhaseStage(Enum):
+    CIVILIANS = auto()
+    MAFIA = auto()
+
+
 class MafiaGame:
     def __init__(self, mafia_coefficient: int = 4, banned_roles: list = None, logger=None, loglevel=logging.DEBUG):
-        self.__token = ''.join(choices(ascii_uppercase + digits, k=GAME_TOKEN_LENGTH))
+        self.token = ''.join(choices(ascii_uppercase + digits, k=GAME_TOKEN_LENGTH))
+        self.input_messages_queue = PriorityQueue()
+
         self.__players = defaultdict()
         self.__mafia_count = 0
         self.__phase = GamePhase.DAY
         self.__mafia_coefficient = max(mafia_coefficient, 1)
         self.__role_factory = RoleFactory()
 
-        self.__input_messages_queue = PriorityQueue()
-
         self.__available_roles = defaultdict(bool, ((role, True) for role in RoleEnum))
         if banned_roles is not None:
             for role in banned_roles:
                 self.__available_roles[role] = False
 
-        self.logger = logger
-        if self.logger is None:
-            self.logger = logging.getLogger("MafiaGame_{token}".format(token=self.__token))
-            self.logger.setLevel(loglevel)
+        self.__logger = logger
+        if self.__logger is None:
+            self.__logger = logging.getLogger("MafiaGame_{token}".format(token=self.token))
+            self.__logger.setLevel(loglevel)
             formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+            ch = logging.StreamHandler()
+            ch.setFormatter(formatter)
+            self.__logger.addHandler(ch)
+
+        self.__logger.info("Created game session `{token}`. Mafias count = N // k, where k is {k}."
+                           " Available roles: {roles}"
+                           .format(token=self.token,
+                                   k=self.__mafia_coefficient,
+                                   roles=", ".join([k for k, v in self.__available_roles.items() if v])))
 
     def __calculate_max_mafia_count(self):
         return len(self.__players) // self.__mafia_coefficient
@@ -67,13 +87,19 @@ class MafiaGame:
 
         mafia_team_roles = self.__get_roles_by_team(roles, TeamEnum.MAFIA)
         mafias = self.__get_packed_roles(mafia_team_roles, self.__mafia_count)
+        self.__logger.debug("Mafia roles to assign: {mafias}".format(
+            mafias=", ".join([role.get_name().name for role in mafias])))
 
         civilians_team_roles = self.__get_roles_by_team(roles, TeamEnum.CIVILIAN)
         civilians_count = max(len(self.__players) - self.__mafia_count, 0)
         civilians = self.__get_packed_roles(civilians_team_roles, civilians_count)
+        self.__logger.debug("Civilians roles to assign: {civilians}".format(
+            civilians=", ".join([role.get_name().name for role in civilians])))
 
         result = mafias + civilians
         shuffle(result)
+        self.__logger.debug("Shuffled roles to assign: {shuffled}".format(
+            shuffled=", ".join([role.get_name().name for role in result])))
         return result
 
     def __assign_roles(self):
@@ -82,8 +108,16 @@ class MafiaGame:
             if len(roles) > 0:
                 player.set_role(roles.pop().get_name())
 
-    def get_token(self):
-        return self.__token
+    def __switch_phase(self):
+        current_role = self.__phase
+        if self.__phase is GamePhase.DAY:
+            self.__phase = GamePhase.NIGHT
+        else:
+            self.__phase = GamePhase.DAY
+        self.__logger.info("Game phase switched {prev}->{curr}".format(prev=current_role.name, curr=self.__phase.name))
+
+    def __game_loop(self):
+        pass
 
     def run(self):
         pass
